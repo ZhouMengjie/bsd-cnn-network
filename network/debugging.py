f65f94c4267ade05a5b5125ddf4558f2f5fd264b
Mengjie Zhou <mengjiezhou2018@outlook.com>
2019-11-01 13:46:58 +0000

    rearrangement

---
+++ network/debugging.py
@@ -0,0 +1,101 @@
+import argparse
+import os
+import shutil
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+
+import wideresnet
+import pdb
+
+model_names = sorted(name for name in models.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
+
+parser = argparse.ArgumentParser(description='PyTorch Places365 Training')
+parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18',
+                    help='model architecture: ' +
+                        ' | '.join(model_names) +
+                        ' (default: resnet18)')
+parser.add_argument('-j', '--workers', default=6, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=256, type=int,
+                    metavar='N', help='mini-batch size (default: 256)')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--print-freq', '-p', default=10, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained', dest='pretrained', action='store_false',
+                    help='use pre-trained model')
+parser.add_argument('--num_classes',default=2, type=int, help='num of class in the model')
+parser.add_argument('--dataset',default='places365',help='which dataset to train')
+
+best_prec1 = 0
+
+args = parser.parse_args()
+print(args)
+
+'''
+# load the pre-trained weights
+model_file = '%s_places365.pth.tar' % args.arch
+if not os.access(model_file, os.W_OK):
+    weight_url = 'http://places2.csail.mit.edu/models_places365/' + model_file
+    os.system('wget ' + weight_url)
+
+model = models.__dict__[args.arch](num_classes=args.num_classes)
+checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage)
+# checkpoint = torch.load(model_file) 
+state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+state_dict = {str.replace(k,'fc.bias' ,'fc1.bias'): v for k,v in state_dict.items()}
+state_dict = {str.replace(k,'fc.weight' ,'fc1.weight'): v for k,v in state_dict.items()}
+model.load_state_dict(state_dict, strict=False)
+# model = torch.nn.DataParallel(model).cuda()
+print(model) 
+'''   
+
+# Data loading code
+data = 'places365_standard'
+traindir = os.path.join(data, 'train')
+valdir = os.path.join(data, 'val')
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225])
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.ImageFolder(traindir, transforms.Compose([
+        transforms.RandomSizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize,
+    ])),
+    batch_size=args.batch_size, shuffle=True,
+    num_workers=args.workers, pin_memory=True)
+
+val_loader = torch.utils.data.DataLoader(
+    datasets.ImageFolder(valdir, transforms.Compose([
+        transforms.Scale(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize,
+    ])),
+    batch_size=args.batch_size, shuffle=False,
+    num_workers=args.workers, pin_memory=True)
