3cd28c3ccd757430cf916604c26864f2f611990f
Mengjie Zhou <mengjiezhou2018@outlook.com>
2019-12-02 14:04:29 +0000

    generate test.txt

---
+++ dataset_split/images/GAPS/save_txt.m
@@ -0,0 +1,23 @@
+% output .txt file
+% read filepath
+clear all
+outfolder = ['test/','gaps'];
+outfolder_n = ['test/','non_gaps'];
+files = dir([outfolder '/*.jpg']);
+files = {files.name}';
+
+files_n = dir([outfolder_n '/*.jpg']);
+files_n = {files_n.name}';
+
+fp = fopen('test.txt','w');
+for i=1:length(files)
+    filepath = [outfolder, '/',files{i}];
+    fprintf(fp,['%s','\n'],filepath);   
+end
+
+for i=1:length(files_n)
+    filepath = [outfolder_n, '/',files_n{i}];
+    fprintf(fp,['%s','\n'],filepath);   
+end
+
+fclose(fp);
