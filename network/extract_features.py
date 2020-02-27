41a984b0ccb3d0b52601addba961fda407469a0f
0b3d <samanoabonce@gmail.com>
2020-02-27 17:07:45 +0000

    update again!

--- network/extract_features.py
+++ network/extract_features.py
@@ -76,9 +76,9 @@ def main():
     print(args)
 
     # load model
-    # classes = ('junctions', 'non_junctions')
-    classes = ('gaps', 'non_gaps')
-    model_file = 'model/GAPS/resnet18_best.pth.tar'
+    classes = ('junctions', 'non_junctions')
+    # classes = ('gaps', 'non_gaps')
+    model_file = 'model/JUNCTIONS/resnet18_best.pth.tar'
     # model_file = 'old_models/gaps_best.pth.tar'
     
     model = models.__dict__[args.arch](num_classes=args.num_classes)
@@ -93,7 +93,7 @@ def main():
     model = model.to(device)
 
     # Data loading code
-    data_dir = 'data/GAPS' # or GAPS
+    data_dir = 'data/JUNCTIONS' # or GAPS
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                     std=[0.229, 0.224, 0.225])   
 
@@ -123,11 +123,11 @@ def main():
     features, panoids = validate(val_loader, model, criterion, classes, features, img_paths, panoids)
     #print(features)
     
-    # scipy.io.savemat('uq_junctions_features.mat', mdict={'features': features})
-    # scipy.io.savemat('uq_junctions_ids_labels.mat', mdict={'img_paths': img_paths})
+    scipy.io.savemat('uq_junctions_features.mat', mdict={'features': features})
+    scipy.io.savemat('uq_junctions_ids_labels.mat', mdict={'panoids': img_paths})
 
-    scipy.io.savemat('uq_gaps_features.mat', mdict={'features': features})
-    scipy.io.savemat('uq_gaps_ids_labels.mat', mdict={'panoids': panoids})
+    # scipy.io.savemat('uq_gaps_features.mat', mdict={'features': features})
+    # scipy.io.savemat('uq_gaps_ids_labels.mat', mdict={'panoids': panoids})
     
 
 
