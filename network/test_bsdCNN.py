7a88e0ee3008c2b52a0af6f375948a5f5eb5c093
Mengjie Zhou <mengjiezhou2018@outlook.com>
2020-04-16 00:20:28 +0100

    test hd gap

--- network/test_bsdCNN.py
+++ network/test_bsdCNN.py
@@ -75,7 +75,7 @@ def main():
     print(args)
 
     # load model
-    model_file = 'model_junction/resnet18_best.pth.tar'
+    model_file = 'model_gap/resnet18_best.pth.tar'
     model = models.__dict__[args.arch](num_classes=args.num_classes)
     checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage) # load to CPU
     state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
@@ -88,10 +88,10 @@ def main():
     model = model.to(device)
 
     # Data loading code
-    # data_dir = 'data/JUNCTIONS' # or GAPS
+    data_dir = 'data/GAPS' # or GAPS
     # data_dir = 'data_onlypositive/JUNCTIONS' # or GAPS
-    data_dir = 'data_onlynegative/JUNCTIONS' # or GAPS
-    valdir = os.path.join(data_dir, 'unionsquare5k')
+    # data_dir = 'data_onlynegative/JUNCTIONS' # or GAPS
+    valdir = os.path.join(data_dir, 'hudsonriver5k')
 
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
