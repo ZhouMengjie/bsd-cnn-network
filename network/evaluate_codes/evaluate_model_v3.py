4be88ba76a6645513582bc89ed47191ba9a6a20a
Mengjie Zhou <mengjiezhou2018@outlook.com>
2020-05-04 19:05:58 +0100

    evaluate gap ws

--- network/evaluate_codes/evaluate_model_v3.py
+++ network/evaluate_codes/evaluate_model_v3.py
@@ -74,11 +74,11 @@ def main():
     # print(args)
 
     # Data loading code
-    data_dir = 'data/JUNCTIONS' # or GAPS
+    data_dir = 'data/GAPS' # or GAPS
     valdir = os.path.join(data_dir, 'wallstreet5k')
-    main_directory = 'model_junction/'
-    ROC_names = 'ROC_jc_ws.png'
-    PR_names = 'PR_jc_ws.png'
+    main_directory = 'model_gap/'
+    ROC_names = 'ROC_bd_ws.png'
+    PR_names = 'PR_bd_ws.png'
 
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
