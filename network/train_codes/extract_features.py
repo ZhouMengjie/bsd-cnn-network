d7cd4c117063b71b8fa6ab176e9f9728e42771b4
Mengjie Zhou <mengjiezhou2018@outlook.com>
2020-05-11 01:26:31 +0100

    uq bd features

--- network/train_codes/extract_features.py
+++ network/train_codes/extract_features.py
@@ -74,10 +74,10 @@ def main():
     # print(args)
 
     # load model
-    classes = ('junctions', 'non_junctions')
-    # classes = ('gaps', 'non_gaps')
-    model_file = 'model_junction_alexnet/alexnet_recall.pth.tar'
-    # model_file = 'model_gap/resnet18_recall.pth.tar'
+    # classes = ('junctions', 'non_junctions')
+    classes = ('gaps', 'non_gaps')
+    # model_file = 'model_junction_alexnet/alexnet_recall.pth.tar'
+    model_file = 'model_gap/alexnet_accuracy.pth.tar'
     
     model = models.__dict__[args.arch](num_classes=args.num_classes)
     checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage) # load to CPU
@@ -91,7 +91,7 @@ def main():
     model = model.to(device)
 
     # Data loading code
-    data_dir = 'data/JUNCTIONS' # or GAPS,JUNCTIONS
+    data_dir = 'data/GAPS' # or GAPS,JUNCTIONS
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                     std=[0.229, 0.224, 0.225]) 
     if args.arch is "alexnet":  
@@ -121,11 +121,11 @@ def main():
     features, panoids = validate(val_loader, model, criterion, classes, features, img_paths, panoids)
 
     
-    scipy.io.savemat('uq_junctions_features.mat', mdict={'features': features})
-    scipy.io.savemat('uq_junctions_ids_labels.mat', mdict={'panoids': panoids})
+    # scipy.io.savemat('uq_junctions_features.mat', mdict={'features': features})
+    # scipy.io.savemat('uq_junctions_ids_labels.mat', mdict={'panoids': panoids})
 
-    # scipy.io.savemat('hd_gaps_features.mat', mdict={'features': features})
-    # scipy.io.savemat('hd_gaps_ids_labels.mat', mdict={'panoids': panoids})
+    scipy.io.savemat('uq_gaps_features.mat', mdict={'features': features})
+    scipy.io.savemat('uq_gaps_ids_labels.mat', mdict={'panoids': panoids})
     
 
 
