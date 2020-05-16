df8f2df0050623d541b73af8fc2f7095cfb1a51e
Mengjie Zhou <mengjiezhou2018@outlook.com>
2020-05-16 21:20:06 +0100

    vgg gap

--- network/train_codes/finetune_placesCNN_val.py
+++ network/train_codes/finetune_placesCNN_val.py
@@ -72,7 +72,7 @@ parser.add_argument('--num_save', default=0, type=int, metavar='N',
 parser.add_argument('--num_checkpoints', default=5, type=int, metavar='N',
                     help='number of saved checkpoints')
 
-writer = SummaryWriter('runs/vgg/bd_all_index')
+writer = SummaryWriter('runs/vgg/jc_all_index')
 if torch.cuda.is_available():
     device = torch.device('cuda:0')
     torch.backends.cudnn.benchmark = True
@@ -149,7 +149,7 @@ def main():
     model = model.to(device)
 
     # Data loading code
-    data_dir = 'data/GAPS' # or GAPS
+    data_dir = 'data/JUNCTIONS' # JUNCTIONS or GAPS
     traindir = os.path.join(data_dir, 'train')
     valdir = os.path.join(data_dir, 'hudsonriver5k')
 
