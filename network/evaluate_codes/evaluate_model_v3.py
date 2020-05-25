cf219e96087aead808c3ac0c4da346f190d21237
Mengjie Zhou <mengjiezhou2018@outlook.com>
2020-05-25 15:38:43 +0100

    evaluate ws jc

--- network/evaluate_codes/evaluate_model_v3.py
+++ network/evaluate_codes/evaluate_model_v3.py
@@ -72,7 +72,7 @@ def main():
 
     # Data loading code
     data_dir = 'data/JUNCTIONS' # JUNCTIONS or GAPS
-    valdir = os.path.join(data_dir, 'hudsonriver5k')
+    valdir = os.path.join(data_dir, 'wallstreet5k')
     main_directory = 'model_junction_densenet161/'
     # ROC_names = 'ROC_jc_uq.png'
     # PR_names = 'PR_jc_uq.png'
