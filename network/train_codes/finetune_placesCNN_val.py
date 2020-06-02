cf02e317e67fd999df147effcc153174dd7d3cdb
Mengjie Zhou <mengjiezhou2018@outlook.com>
2020-06-02 20:30:17 +0100

    lr = 0.01

--- network/train_codes/finetune_placesCNN_val.py
+++ network/train_codes/finetune_placesCNN_val.py
@@ -50,7 +50,7 @@ parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N', help='mini-batch size (default: 256)')
-parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+parser.add_argument('--lr', '--learning-rate', default=0.01, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
